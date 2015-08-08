a58028d9f049f67f4a00b320c2fbeda58880faa8
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-08-08 15:59:26 -0700

    Added 4 queens test
    Should be at release one when the package can solve this.

--- test/aruspex_test.exs
+++ test/aruspex_test.exs
@@ -1,7 +1,38 @@
 defmodule AruspexTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  @tag timeout: 1000
+  test "4 queens" do
+    {:ok, pid} = Aruspex.new
+
+    variables = [W, X, Y, Z]
+    pid |> Aruspex.variables(variables)
+    pid |> Aruspex.domain(variables, (for i <- 1..4, j <- 1..4, do: {i,j}))
+
+    queen_constraints(pid, variables)
+
+    labels = pid |> Aruspex.label
+
+    assert labels[W] == {1,2}
+    assert labels[X] == {2,4}
+    assert labels[Y] == {3,1}
+    assert labels[Z] == {4,3}
+  end
+
+  def queen_constraints(_pid, [_x]), do: {:ok}
+
+  def queen_constraints(pid, [x|t]) do
+    for y <- t do
+      pid |> Aruspex.constraint [x, y], fn
+        (s, s) -> false
+        ({s, _x2}, {s, _y2}) -> false
+        ({_x1, s}, {_y1, s}) -> false
+        ({x1, x2}, {y1, y2}) when x1+x2 == y1+y2 -> false
+        ({x1, x2}, {y1, y2}) when x1-x2 == y1-y2 -> false
+        (_, _) -> true
+      end
+    end
+
+    queen_constraints(pid, t)
   end
 end
