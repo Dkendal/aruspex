aec6ba48763800562aa23763c113d2a914c66ce8
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-08-08 16:35:03 -0700

    Defined interface for solver

--- lib/aruspex.ex
+++ lib/aruspex.ex
@@ -1,2 +1,21 @@
 defmodule Aruspex do
+  use GenServer
+
+  def new(default\\[])
+
+  def new(default) do
+    GenServer.start_link(__MODULE__, default)
+  end
+
+  def variables(pid, variables) do
+  end
+
+  def domain(pid, variables, domain) do
+  end
+
+  def constraint(pid, variables, constraint) do
+  end
+
+  def label(pid) do
+  end
 end
