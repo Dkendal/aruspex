29846e36f73d05503e25184c72df9d292f1aa361
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-08-08 17:34:51 -0700

    Added `variables`, and `domain` method

--- lib/aruspex.ex
+++ lib/aruspex.ex
@@ -1,21 +1,50 @@
 defmodule Aruspex do
   use GenServer
+  import GenServer
+  import Enum, only: [reduce: 3]
 
-  def new(default\\[])
+  defmodule Var do
+    defstruct binding: nil, constraints: [], domain: []
+    @type constraint :: ((any, any) -> boolean)
+    @type t :: %Var{binding: any, constraints: [constraint], domain: Enum.t }
+  end
 
-  def new(default) do
-    GenServer.start_link(__MODULE__, default)
+  # Client
+
+  def new(options\\[])
+
+  def new(options) do
+    start_link(__MODULE__, %{}, options)
   end
 
   def variables(pid, variables) do
+    pid |> cast({:variables, variables})
   end
 
   def domain(pid, variables, domain) do
+    pid |> cast({:domain, variables, domain})
   end
 
   def constraint(pid, variables, constraint) do
   end
 
   def label(pid) do
+  end
+
+  # Callbacks
+  @doc "variables/1 callback"
+  def handle_cast({:variables, variables}, state) do
+    new_state = reduce variables, state, fn(v, acc) ->
+      put_in acc, [v], %Var{}
+    end
+    {:noreply, new_state}
+  end
+
+  @doc "domain/2 callback"
+  def handle_cast({:domain, variables, domain}, state) do
+    new_state = reduce variables, state, fn(v, acc) ->
+      update_in acc, [v, :domain], domain
+    end
+    {:noreply, new_state}
   end
 end
