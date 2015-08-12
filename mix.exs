1e115b91d158c14dab056a47530a233915779323
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-08-11 19:45:56 -0700

    Added dbg

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Aruspex.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :dbg]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,6 @@ defmodule Aruspex.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [exactor: "~> 2.1.0"]
+    [{:exactor, "~> 2.1.0"}, {:dbg, github: "fishcakez/dbg"}]
   end
 end
