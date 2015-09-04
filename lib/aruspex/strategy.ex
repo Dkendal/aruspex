4d2c184ce35f700e50d21dbbc35db451107ac507
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-09-03 22:19:38 -0700

    Aliased state in strategy file

--- lib/aruspex/strategy.ex
+++ lib/aruspex/strategy.ex
@@ -1,6 +1,7 @@
 defmodule Aruspex.Strategy do
   use Behaviour
+  alias Aruspex.State
 
   @doc "Binds values to variables that statisfy defined constraints."
-  defcallback label(state :: Aruspex.State) :: Aruspex.State
+  defcallback label(state :: State) :: State
 end
