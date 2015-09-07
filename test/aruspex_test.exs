20a3a07a176a67425baae9bb5e290fb4c5978927
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-09-07 16:41:44 -0700

    Moved queens test out of a macro
    Just make it an actual test.

--- test/aruspex_test.exs
+++ test/aruspex_test.exs
@@ -1,43 +1,3 @@
-defmodule QueenTest do
-  use Aruspex.Constraint
-
-  alias Aruspex, as: A
-
-  defmacro run strategy, queens do
-    quote do
-      @tag timeout: 100000
-      test "#{unquote queens} queens" do
-        n = unquote queens
-        strategy = unquote strategy
-
-        {:ok, pid} = A.start_link
-
-        for x <- 1..n, do:
-          A.variable(pid, x, (for y <- 1..n, do: {x, y}))
-
-        A.set_strategy pid, strategy
-
-        for_all pid, fn
-          (s, s) -> 1
-          ({s, _x2}, {s, _y2}) -> 1
-          ({_x1, s}, {_y1, s}) -> 1
-          ({x1, x2}, {y1, y2}) when x1+x2 == y1+y2 -> 1
-          ({x1, x2}, {y1, y2}) when x1-x2 == y1-y2 -> 1
-          (_, _) -> 0
-        end
-
-        pid |> A.label
-        state = pid |> A.get_state
-
-        assert unquote((queens * queens - queens) / 2) ==
-          length state.constraints
-
-        assert 0 == state.cost
-      end
-    end
-  end
-end
-
 defmodule AruspexTest do
   use ExUnit.Case, async: true
   doctest Aruspex
@@ -78,7 +38,33 @@ end
 
 defmodule Aruspex.Strategy.SimulatedAnnealingTest do
   use ExUnit.Case, async: true
-  require QueenTest
+  use Aruspex.Constraint
+  alias Aruspex, as: A
 
-  QueenTest.run Aruspex.Strategy.SimulatedAnnealing, 20
+  @tag timeout: 100000
+  test "4 queens" do
+    n = 4
+    strategy = :simulated_annealing
+
+    {:ok, pid} = A.start_link
+
+    for x <- 1..n, do:
+    A.variable(pid, x, (for y <- 1..n, do: {x, y}))
+    A.set_strategy pid, Aruspex.Strategy.SimulatedAnnealing
+
+    for_all pid, fn
+      (s, s) -> 1
+      ({s, _x2}, {s, _y2}) -> 1
+      ({_x1, s}, {_y1, s}) -> 1
+      ({x1, x2}, {y1, y2}) when x1+x2 == y1+y2 -> 1
+      ({x1, x2}, {y1, y2}) when x1-x2 == y1-y2 -> 1
+      (_, _) -> 0
+    end
+
+    A.label pid
+    state = A.get_state pid
+
+    assert (n * n - n) / 2 == length state.constraints
+    assert 0 == state.cost
+  end
 end
