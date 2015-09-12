9b76c32354c4626c313db55cb45c72a58f27dffd
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-09-11 22:52:36 -0700

    Added friendly errors when strategies fail
    In the event that a strategy returns a nil value report an error and
    fail fast. If not there will be a less helpfull nil.domain() undefined
    error later

--- lib/aruspex/server.ex
+++ lib/aruspex/server.ex
@@ -54,6 +54,11 @@ defmodule Aruspex.Server do
   @spec find_solution(pid) :: [{var, any}]
   defcall find_solution(), state: state do
     state.options.strategy.label(state)
+    |> case do
+      nil ->
+        raise Aruspex.Strategy.InvalidResultError, module: state.options.strategy
+      s -> s
+    end
     |> tap s ~> set_and_reply s, bound_variables(s)
   end
 
