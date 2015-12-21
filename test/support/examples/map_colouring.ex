b1d28d61701e7ad1b9ee3f683a6568fe1ca7cb66
Dylan Kendal <dylan@stembolt.com>
2015-12-20 20:09:05 -0800

    Removed fake bound variable in coloring test

--- test/support/examples/map_colouring.ex
+++ test/support/examples/map_colouring.ex
@@ -19,15 +19,6 @@ defmodule Examples.MapColouring do
 
         domain = [:red, :green, :blue]
 
-        # Adds a constrained variables that will attempt to minimize the number
-        # of colours used.
-        # The cost of the constraint is the number of unique
-        # colors used.
-        minimize_colours_used = constraint(
-          variables: variables,
-          function: &(Enum.uniq(&1) |> length)
-        )
-
         Enum.map variables, &(variable problem, &1, domain)
 
         problem
@@ -42,7 +33,6 @@ defmodule Examples.MapColouring do
         |> post(linear ^nt != ^q)
         |> post(linear ^q != ^nsw)
         |> post(linear ^nsw != ^v)
-        |> post(minimize_colours_used)
         |> find_solution
 
         # minimum colouring is 3, a value greater than three indicates that a
@@ -50,7 +40,7 @@ defmodule Examples.MapColouring do
         # TODO replace this with a constrained variable and clearer validity
         # checking
         cost = Aruspex.State.get_cost :sys.get_state problem
-        assert cost == 3
+        assert cost == 0
       end
     end
   end
