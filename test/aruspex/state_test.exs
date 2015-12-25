ca4494ed856d348a065bb8158a6c321f98f957a5
Dylan Kendal <dylan@stembolt.com>
2015-12-24 22:25:50 -0800

    Rewrote compute_cost test

--- test/aruspex/state_test.exs
+++ test/aruspex/state_test.exs
@@ -1,34 +1,33 @@
 defmodule Aruspex.StateTest do
-  use ExSpec, async: true
+  alias Aruspex.State
+  alias Aruspex.Var
+  use Aruspex.Case
   use Aruspex.Constraint
 
   describe "compute_cost/1" do
     setup do
-      variables = [:x, :y]
+      variables = 1..10
 
-      {:ok, pid} = Aruspex.Server.start_link
+      state = struct Aruspex.State
 
-      for v <- variables, do: Aruspex.Server.variable(pid, v, [1])
+      state = Enum.reduce(variables, state, fn v, state ->
+        state = State.set_var(state, v, [1] |> Var.new |> Var.bind(1))
 
-      pid
-      |> Aruspex.Server.post(linear ^:y != ^:x)
-      |> Aruspex.Server.post(linear ^:x != 1)
-      |> Aruspex.Server.post(linear ^:y != 1)
+        Enum.reduce(variables, state, fn w, state ->
+          if v > w do
+            State.add_constraint(state, linear ^v != ^w)
+          else
+            state
+          end
+        end)
+      end)
 
-      state = :sys.get_state(pid)
-      Aruspex.Server.stop pid
+      state = State.compute_cost(state)
 
-      state = put_in state.variables.x.binding, 1
-      state = put_in state.variables.y.binding, 1
-
-      {:ok,
-        subject: Aruspex.State.compute_cost(state)}
+      {:ok, %{state: state}}
     end
 
     it "returns the cost, and an updated state", c do
-      assert c.subject.cost == 3
     end
   end
 end
-
-
