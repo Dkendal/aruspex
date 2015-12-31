dea1681d5e2bcb214edf4d9dcbe8f74fa1e2ae8c
Dylan Kendal <dylan@stembolt.com>
2015-12-30 21:59:16 -0800

    Log stats on strategy tests

--- test/aruspex/strategy/dfs_test.exs
+++ test/aruspex/strategy/dfs_test.exs
@@ -6,6 +6,8 @@ defmodule Aruspex.Strategy.DfsTest do
     |> Aruspex.Strategy.Dfs.set_strategy()
     |> Enum.take(1)
 
+    Aruspex.Logger.log_stats([result])
+
     assert result.valid? == true
   end
 end
--- test/aruspex/strategy/simulated_annealing_test.exs
+++ test/aruspex/strategy/simulated_annealing_test.exs
@@ -6,6 +6,8 @@ defmodule Aruspex.Strategy.SimulatedAnnealingTest do
     |> Aruspex.Strategy.SimulatedAnnealing.set_strategy()
     |> Enum.take(1)
 
+    Aruspex.Logger.log_stats([result])
+
     assert result.valid? == true
   end
 end
