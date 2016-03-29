5ea54c630cb308881b1308be08b321e484673b1d
Dylan Kendal <dylan@stembolt.com>
2016-03-29 20:32:11 +0700

    Add choose to ac3

--- lib/aruspex/strategy/ac3.ex
+++ lib/aruspex/strategy/ac3.ex
@@ -1,18 +1,34 @@
 defmodule Aruspex.Strategy.Ac3 do
   @behaviour Aruspex.Strategy
 
+  import Aruspex.Problem
+
   defstruct problem: nil, timeout: 5000
 
+  @doc """
+  Set the search strategy as AC3 Arc Consistency.
+  """
   def set_strategy(problem, opts \\ []) do
     opts = Enum.into opts, %{}
     struct __MODULE__, Map.put(opts, :problem, problem)
   end
 
-  defimpl Enumerable, for: __MODULE__ do
-    use Aruspex.Strategy
+  def choose(evaluation) do
+    hd variables evaluation.problem
+  end
+end
 
-    def reduce(s, {:cont, acc}, fun) do
-      { :done, acc }
-    end
+defimpl Enumerable, for: Aruspex.Strategy.Ac3 do
+  use Aruspex.Strategy
+  import Aruspex.Strategy.Ac3
+
+  # if complete
+  #   return result
+  # choose an unbound variable v
+  # select a value from the domain
+  # collapse domain to v
+  # for each edge, collapse domains to values that hold true
+  def reduce(s, {:cont, acc}, fun) do
+    { :done, acc }
   end
 end
--- test/aruspex/strategy/ac3_test.exs
+++ test/aruspex/strategy/ac3_test.exs
@@ -1,11 +1,10 @@
 defmodule Aruspex.Strategy.Ac3Test do
   use Aruspex.Case
 
-  test "solves basic constraints" do
+  setup do
     import Aruspex.Problem
     problem = new
 
-    assignments =
     problem
     |> add_variable(:x, 1..10)
     |> add_variable(:y, 1..10)
@@ -13,9 +12,19 @@ defmodule Aruspex.Strategy.Ac3Test do
     |> post([:x, :y, :z], fn x, y, z ->
       x + y == z
     end)
-    |> Aruspex.Strategy.Ac3.set_strategy([])
-    |> Enum.take(2)
 
-    IO.inspect assignments
+    assignment = %Aruspex.Evaluation{problem: problem}
+
+    { :ok, problem: problem, assignment: assignment }
+  end
+
+  test "solves basic constraints", %{problem: problem} do
+    import Aruspex.Problem
+  end
+
+  describe "choose/1" do
+    it "returns an unbound variable", %{ assignment: assignment } do
+      assert Aruspex.Strategy.Ac3.choose(assignment) in [:x, :y, :z]
+    end
   end
 end
