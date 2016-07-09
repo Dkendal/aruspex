e011f62f145564efaebe964de5aae3ba09e987f7
Dylan Kendal <dylan@stembolt.com>
2016-07-09 15:11:33 -0700

    upgrade to elixir 1.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Aruspex.Mixfile do
     [
       app: :aruspex,
       version: "0.1.0",
-      elixir: "~> 1.2",
+      elixir: "~> 1.3",
       description: description,
       package: package,
       build_embedded: Mix.env == :prod,
