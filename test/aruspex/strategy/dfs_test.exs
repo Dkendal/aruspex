9a28745ef0e9d59863881d5ebd762ebe5a26418b
Dylan Kendal <dylan@stembolt.com>
2016-07-09 16:08:41 -0700

    Addmore tests to the dfs map coloring example

--- test/aruspex/strategy/dfs_test.exs
+++ test/aruspex/strategy/dfs_test.exs
@@ -2,10 +2,33 @@ defmodule Aruspex.Strategy.DfsTest do
   use Aruspex.Case
 
   test "solves the map colouring problem" do
-    results = Examples.MapColouring.new
-    |> Aruspex.Strategy.Dfs.set_strategy()
-    |> Enum.take(2)
+    territories = [
+      :western_australia,
+      :nothern_territory,
+      :queensland,
+      :south_australia,
+      :new_south_wales,
+      :victoria,
+      :tasmania
+    ]
 
-    assert (hd results).valid? == true
+    colors = [:red, :green, :blue]
+
+    result = Examples.MapColouring.new
+              |> Aruspex.Strategy.Dfs.set_strategy()
+              |> Enum.take(1)
+              |> hd
+
+    assert result.valid? == true
+
+    assert map_size(result.binding) == 7
+
+    assert MapSet.subset?(
+      MapSet.new(territories),
+      MapSet.new(Dict.keys(result.binding)))
+
+     assert MapSet.subset?(
+       MapSet.new(colors),
+       MapSet.new(Dict.values(result.binding)))
   end
 end
